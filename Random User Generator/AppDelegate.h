8a2e7e8489ce60fa5424d0ec6b43d456fd2164d2
Greg Barbosa <gregbarbosa@me.com>
2014-01-30 00:47:07 -0500

    Initial Commit

---
+++ Random User Generator/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Random User Generator
+//
+//  Created by Greg Barbosa on 1/30/14.
+//  Copyright (c) 2014 PolaritySoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
